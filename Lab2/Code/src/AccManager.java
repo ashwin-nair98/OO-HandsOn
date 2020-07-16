a7cc254891c423faea2312a4267983a5b68a567e
Ashwin B Nair <ashwin.bala.432@gmail.com>
2020-07-16 12:05:36 +0530

    Lab2 completed and report added

--- Lab2/Code/src/AccManager.java
+++ Lab2/Code/src/AccManager.java
@@ -2,6 +2,15 @@ public class AccManager {
 	public void fillAccountData(Account a1) {
 	
 	//Complete the statements
+		System.out.println("Enter acc no: ");
+		String accNo = Console.readLine();
+		System.out.println("Enter name: ");
+		String name = Console.readLine();
+		System.out.println("Enter balance: ");
+		double balance = Console.readDouble(); 
+		a1.setName(name);
+		a1.setBalance(balance);
+		a1.setAccNo(accNo);
 
 	}
 	public void displayAccountData(Account a1) {
